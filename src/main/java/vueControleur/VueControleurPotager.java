5b120a597187ef4ccbc8075e3b74e3028b98d7b1
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-03-22 19:57:11 +0100

    Refactor

--- src/main/java/vueControleur/VueControleurPotager.java
+++ src/main/java/vueControleur/VueControleurPotager.java
@@ -12,6 +12,8 @@ import java.awt.event.MouseEvent;
 import java.util.Observable;
 import java.util.Observer;
 
+import static vueControleur.IconNames.*;
+
 
 /**
  * Cette classe a deux fonctions :
@@ -19,17 +21,25 @@ import java.util.Observer;
  * (2) Controleur : écouter les évènements clavier et déclencher le traitement adapté sur le modèle
  */
 public class VueControleurPotager extends JFrame implements Observer {
+    // Window properties
+    public static final int WIDTH = 540;
+    public static final int HEIGHT = 250;
+    public static final String WINDOW_TITLE = "A vegetable garden - Potager";
+
     private final IconRepository icones = IconRepository.getInstance();
-    private SimulateurPotager simulateurPotager; // référence sur une classe de modèle : permet d'accéder aux données du modèle pour le rafraichissement, permet de communiquer les actions clavier (ou souris)
-    private int sizeX; // taille de la grille affichée
-    private int sizeY;
+    private final SimulateurPotager simulateurPotager; // référence sur une classe de modèle : permet d'accéder aux données du modèle pour le rafraichissement, permet de communiquer les actions clavier (ou souris)
+
+    // taille de la grille affichée
+    private final int sizeX;
+    private final int sizeY;
+
     private JLabel[][] tabJLabel; // cases graphiques (au moment du rafraichissement, chaque case va être associée à une icône, suivant ce qui est présent dans le modèle)
 
 
-    public VueControleurPotager(SimulateurPotager _simulateurPotager) {
+    public VueControleurPotager(SimulateurPotager simPota) {
         sizeX = SimulateurPotager.SIZE_X;
         sizeY = SimulateurPotager.SIZE_Y;
-        simulateurPotager = _simulateurPotager;
+        simulateurPotager = simPota;
 
         placerLesComposantsGraphiques();
         //ajouterEcouteurClavier(); // si besoin
@@ -52,10 +62,14 @@ public class VueControleurPotager extends JFrame implements Observer {
 
 
     private void placerLesComposantsGraphiques() {
-        setTitle("A vegetable garden");
-        setSize(540, 250);
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // permet de terminer l'application à la fermeture de la fenêtre
+        initWindow();
 
+        initInfoPanel();
+
+        initCases();
+    }
+
+    private void initInfoPanel() {
         JPanel infos = new JPanel();
 
         JTextField jtf = new JTextField("infos diverses"); // TODO inclure dans mettreAJourAffichage ...
@@ -63,24 +77,9 @@ public class VueControleurPotager extends JFrame implements Observer {
         infos.add(jtf);
 
         add(infos, BorderLayout.EAST);
+    }
 
-
-        JComponent grilleJLabels = new JPanel(new GridLayout(sizeY, sizeX)); // grilleJLabels va contenir les cases graphiques et les positionner sous la forme d'une grille
-
-        tabJLabel = new JLabel[sizeX][sizeY];
-
-        for (int y = 0; y < sizeY; y++) {
-            for (int x = 0; x < sizeX; x++) {
-                JLabel jlab = new JLabel();
-
-                tabJLabel[x][y] = jlab; // on conserve les cases graphiques dans tabJLabel pour avoir un accès pratique à celles-ci (voir mettreAJourAffichage() )
-                grilleJLabels.add(jlab);
-            }
-        }
-        add(grilleJLabels, BorderLayout.CENTER);
-
-        // écouter les évènements
-
+    private void addListenerCases() {
         for (int y = 0; y < sizeY; y++) {
             for (int x = 0; x < sizeX; x++) {
                 final int xx = x; // constantes utiles au fonctionnement de la classe anonyme
@@ -95,41 +94,72 @@ public class VueControleurPotager extends JFrame implements Observer {
         }
     }
 
+    private void initCases() {
+        JComponent grilleJLabels = new JPanel(new GridLayout(sizeY, sizeX)); // grilleJLabels va contenir les cases graphiques et les positionner sous la forme d'une grille
+
+        tabJLabel = new JLabel[sizeX][sizeY];
+
+        for (int y = 0; y < sizeY; y++) {
+            for (int x = 0; x < sizeX; x++) {
+                JLabel jlab = new JLabel();
+
+                tabJLabel[x][y] = jlab; // on conserve les cases graphiques dans tabJLabel pour avoir un accès pratique à celles-ci (voir mettreAJourAffichage() )
+                grilleJLabels.add(jlab);
+            }
+        }
+        add(grilleJLabels, BorderLayout.CENTER);
+
+        addListenerCases();
+    }
+
+    private void initWindow() {
+        setTitle(WINDOW_TITLE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null); // centre la fenêtre
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); // permet de terminer l'application à la fermeture de la fenêtre
+    }
+
 
     /**
      * Il y a une grille du côté du modèle ( jeu.getGrille() ) et une grille du côté de la vue (tabJLabel)
      */
     private void mettreAJourAffichage() {
-
         for (int x = 0; x < sizeX; x++) {
             for (int y = 0; y < sizeY; y++) {
-                if (simulateurPotager.getPlateau()[x][y] instanceof CaseCultivable) { // si la grille du modèle contient un Pacman, on associe l'icône Pacman du côté de la vue
-
-                    Legume legume = ((CaseCultivable) simulateurPotager.getPlateau()[x][y]).getLegume();
-
-                    if (legume != null) {
-
-                        switch (legume.getVariete()) {
-                            case salade:
-                                tabJLabel[x][y].setIcon(icones.getIcone(IconNames.SALADE));
-                                break;
-                        }
-
-                    } else {
-                        tabJLabel[x][y].setIcon(icones.getIcone(IconNames.TERRE));
-                    }
-
-                    // si transparence : images avec canal alpha + dessins manuels (voir ci-dessous + créer composant qui redéfinie paint(Graphics g)), se documenter
-                    //BufferedImage bi = getImage("Images/smick.png", 0, 0, 20, 20);
-                    //tabJLabel[x][y].getGraphics().drawImage(bi, 0, 0, null);
-                } else if (simulateurPotager.getPlateau()[x][y] instanceof CaseNonCultivable) {
-                    tabJLabel[x][y].setIcon(icones.getIcone(IconNames.MUR));
-                } else {
-
-                    tabJLabel[x][y].setIcon(icones.getIcone(IconNames.VIDE));
-                }
+                updateCase(x, y);
             }
         }
+    }
+
+    private void updateCase(int x, int y) {
+        if (simulateurPotager.getPlateau()[x][y] instanceof CaseCultivable) {
+            setLegumeCase(x, y);
+        } else if (simulateurPotager.getPlateau()[x][y] instanceof CaseNonCultivable) {
+            tabJLabel[x][y].setIcon(icones.getIcone(MUR));
+        } else {
+            tabJLabel[x][y].setIcon(icones.getIcone(VIDE));
+        }
+    }
+
+    private void setLegumeCase(int x, int y) {
+        Legume legume = ((CaseCultivable) simulateurPotager.getPlateau()[x][y]).getLegume();
+
+        if (legume != null) {
+            switch (legume.getVariete()) {
+                case SALADE:
+                    tabJLabel[x][y].setIcon(icones.getIcone(SALADE));
+                    break;
+                case CAROTTE:
+                    tabJLabel[x][y].setIcon(icones.getIcone(CAROTTE));
+                    break;
+            }
+        } else {
+            tabJLabel[x][y].setIcon(icones.getIcone(TERRE));
+        }
+
+        // si transparence : images avec canal alpha + dessins manuels (voir ci-dessous + créer composant qui redéfinie paint(Graphics g)), se documenter
+        //BufferedImage bi = getImage("Images/smick.png", 0, 0, 20, 20);
+        //tabJLabel[x][y].getGraphics().drawImage(bi, 0, 0, null);
     }
 
     @Override
