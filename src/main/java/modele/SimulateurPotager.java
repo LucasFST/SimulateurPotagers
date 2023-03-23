ebe9ee6c38fc066972bd4c623109ac375c4b962f
Lucas <lucas.faustmann@gmail.com>
2023-03-23 13:10:36 +0100

    Update class SimulateurPotager

--- src/main/java/modele/SimulateurPotager.java
+++ src/main/java/modele/SimulateurPotager.java
@@ -5,87 +5,41 @@
  */
 package modele;
 
-
+import modele.Potager;
 import modele.environnement.Case;
 import modele.environnement.CaseCultivable;
 import modele.environnement.CaseNonCultivable;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.Random;
 
 
 public class SimulateurPotager {
 
-    public static final int SIZE_X = 20;
-    public static final int SIZE_Y = 10;
-
-    private SimulateurMeteo simMet;
+    private ArrayList<Potager> listePotagers = new ArrayList<Potager>();
+    private SimulateurMeteo simulateurMeteo;
 
     // private HashMap<Case, Point> map = new  HashMap<Case, Point>(); // permet de récupérer la position d'une entité à partir de sa référence
-    private Case[][] grilleCases = new Case[SIZE_X][SIZE_Y]; // permet de récupérer une entité à partir de ses coordonnées
 
     public SimulateurPotager() {
-
-        initialisationDesEntites();
-
-        simMet = new SimulateurMeteo(this);
-
+        simulateurMeteo = new SimulateurMeteo(this);
+        //par défaut, on a un potager
+        ajouterPotager(new Potager());
     }
 
-
-    public Case[][] getPlateau() {
-        return grilleCases;
+    //Pour l'instant public, mais devrait être privé quand on aura un système d'achat de potager
+    //Est-ce qu'on fixe une taille maximale de potagers ?
+    //TODO : ajouter un système d'achat de potager
+    public void ajouterPotager(Potager _potager) {
+        listePotagers.add(_potager);
     }
 
-    private void initialisationDesEntites() {
-
-        // murs extérieurs horizontaux
-        for (int x = 0; x < 20; x++) {
-            addEntite(new CaseNonCultivable(this), x, 0);
-            addEntite(new CaseNonCultivable(this), x, 9);
-        }
-
-        // murs extérieurs verticaux
-        for (int y = 1; y < 9; y++) {
-            addEntite(new CaseNonCultivable(this), 0, y);
-            addEntite(new CaseNonCultivable(this), 19, y);
-        }
-
-        addEntite(new CaseNonCultivable(this), 2, 6);
-        addEntite(new CaseNonCultivable(this), 3, 6);
-
-        Random rnd = new Random();
-
-        for (int x = 5; x < 15; x++) {
-            for (int y = 3; y < 7; y++) {
-                CaseCultivable cc = new CaseCultivable(this);
-                addEntite(cc, x, y);
-                if (rnd.nextBoolean()) {
-                    cc.actionUtilisateur();
-                }
-
-                Ordonnanceur.getInstance().add(cc);
-
-            }
-        }
-
+    public void supprimerPotager(Potager _potager) {
+        listePotagers.remove(_potager);
     }
 
-    public void actionUtilisateur(int x, int y) {
-        if (grilleCases[x][y] != null) {
-            grilleCases[x][y].actionUtilisateur();
-        }
-    }
+    //TODO : ajouter une fonction pour les actions de la météo
 
-    private void addEntite(Case e, int x, int y) {
-        grilleCases[x][y] = e;
-        //map.put(e, new Point(x, y));
-    }
-
-
-    private Case objetALaPosition(Point p) {
-        Case retour = null;
-        return grilleCases[p.x][p.y];
-    }
 
 }
