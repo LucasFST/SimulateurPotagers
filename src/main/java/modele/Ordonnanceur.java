5487f459af8e8940c4dea9f8f0b1da757bee3da3
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-03-23 15:55:07 +0100

    Renamed `Vector<Runnable>` `lst` to `runnables`

--- src/main/java/modele/Ordonnanceur.java
+++ src/main/java/modele/Ordonnanceur.java
@@ -10,7 +10,7 @@ public class Ordonnanceur extends Observable implements Runnable {
     public static final long DEFAULT_DELAY = 10000;
     private static final long UI_DELAY = 1000 / 60; // 1000 / fps
     private static Ordonnanceur ordonnanceur; // singleton
-    private final Vector<Runnable> lst = new Vector<>(); // liste synchronisée
+    private final Vector<Runnable> runnables = new Vector<>(); // liste synchronisée
     Timer timer;
     private long delayMs;
 
@@ -39,12 +39,12 @@ public class Ordonnanceur extends Observable implements Runnable {
     }
 
     public void addRunnable(Runnable r) {
-        lst.add(r);
+        runnables.add(r);
     }
 
     @Override
     public void run() {
-        for (Runnable r : lst) {
+        for (Runnable r : runnables) {
             r.run();
         }
     }
