41cd1e1a78389a103085547ffb78b642e39268af
SIONI Fares <fares.sioni@etu.univ-lyon1.fr>
2023-04-11 06:26:44 +0200

    Renamed files

---
+++ src/main/java/a_vegetable_garden/vue_controleur/vues/components/ActionsButtonsPanel.java
@@ -0,0 +1,129 @@
+package a_vegetable_garden.vue_controleur.vues.components;
+
+import a_vegetable_garden.modele.legumes.Varietes;
+import a_vegetable_garden.modele.potagers.Actions;
+import a_vegetable_garden.vue_controleur.VueManager;
+import a_vegetable_garden.vue_controleur.icon.IconNames;
+import a_vegetable_garden.vue_controleur.icon.IconRepository;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class ActionsButtonsPanel extends Panel {
+    private static Varietes currentVariete = null;
+    private static Actions currentAction = null;
+
+    public static JButton getButton(Actions action) {
+        JButton button;
+        ImageIcon icon;
+        ActionListener actionListener;
+        switch (action) {
+            case PLANTER -> {
+                icon = IconRepository.getInstance().getIcone(IconNames.PLANTER);
+                button = new JButton("Planter", icon);
+                actionListener = e -> planterOnClick();
+            }
+            case ARROSER -> {
+                icon = IconRepository.getInstance().getIcone(IconNames.ARROSER);
+                button = new JButton("Arroser", icon);
+                actionListener = e -> setAction(Actions.ARROSER);
+            }
+            case RECOLTER -> {
+                icon = IconRepository.getInstance().getIcone(IconNames.RECOLTER);
+                button = new JButton("Récolter", icon);
+                actionListener = e -> setAction(Actions.RECOLTER);
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + action);
+        }
+
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.setFont(subPanelFont);
+        button.setEnabled(true);
+        VueManager.getInstance().changeCursor(Actions.RECOLTER);
+        button.addActionListener(actionListener);
+        return button;
+    }
+
+    private static void planterOnClick() {
+        setAction(Actions.PLANTER);
+        // Récupérer les boutons des variétés
+        JButton[] buttons = ButtonsSelectVariete.getInstance().getButtons();
+        // Ouvrir la fenêtre de plantation ici
+        JDialog dialog = new JDialog();
+        dialog.setTitle("Fenêtre de plantation");
+        dialog.setLayout(new GridLayout(0, 1)); // 1 colonne, nombre de lignes automatique
+        for (JButton button : buttons) {
+            dialog.add(button);
+        }
+        // Ajouter un ActionListener à chaque bouton pour fermer le JDialog et récupérer l'index du bouton cliqué
+        for (int i = 0; i < buttons.length; i++) {
+            int finalI = i;
+            buttons[i].addActionListener(e1 -> {
+                dialog.dispose(); // Fermer la boîte de dialogue
+                // Ordre des boutons = ordre des variétés
+                currentVariete = Varietes.values()[finalI];
+            });
+        }
+        // Afficher la boîte de dialogue modale
+        dialog.setModal(true);
+        dialog.pack();
+        dialog.setLocationRelativeTo(null); // Centrer la boîte de dialogue
+        dialog.setVisible(true);
+    }
+
+    protected void initMainPanel() {
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0;
+        mainPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        mainPanel.setBackground(Color.WHITE);
+
+        addPaddingBetweenSubComponents(mainPanel);
+
+        addActionButtonToPanel(c, Actions.PLANTER, 0);
+
+        addPaddingBetweenSubComponents(mainPanel);
+
+        addActionButtonToPanel(c, Actions.ARROSER, 1);
+
+        addPaddingBetweenSubComponents(mainPanel);
+
+        addActionButtonToPanel(c, Actions.RECOLTER, 2);
+    }
+
+    private void addActionButtonToPanel(GridBagConstraints c, Actions action, int gridyIndex) {
+        JButton button = getButton(action);
+        c.gridy = gridyIndex;
+        mainPanel.add(button, c);
+    }
+
+    public JPanel getPanel() {
+        JPanel panel = initPanel(130, 130);
+
+        JLabel panelTitle = initTitle("Actions");
+        panel.add(panelTitle);
+
+        addPaddingBetweenComponents(panel);
+
+        initMainPanel();
+        panel.add(mainPanel);
+
+        return panel;
+    }
+
+    public Actions getAction() {
+        return currentAction;
+    }
+
+    private static void setAction(Actions action) {
+        currentAction = action;
+        VueManager.getInstance().changeCursor(action);
+    }
+
+    public Varietes getCurrentVariete() {
+        return currentVariete;
+    }
+}
--- src/main/java/a_vegetable_garden/vue_controleur/vues/components/ButtonsSelectVariete.java
+++ src/main/java/a_vegetable_garden/vue_controleur/vues/components/ButtonsSelectVariete.java
@@ -10,18 +10,20 @@ import a_vegetable_garden.vue_controleur.icon.IconRepository;
 import javax.swing.*;
 import java.io.Serializable;
 
-public class ButtonsInfosLegume implements Singleton, Serializable {
-    private static ButtonsInfosLegume instance = null;
+public class ButtonsSelectVariete implements Singleton, Serializable {
+    private static ButtonsSelectVariete instance = null;
     private final JButton[] buttons = new JButton[Varietes.values().length];
     private final ImageIcon[] icones = new ImageIcon[Varietes.values().length];
     private final String[] noms = new String[Varietes.values().length];
     private final String[] prix = new String[Varietes.values().length];
 
-    public ButtonsInfosLegume() { chargerButtons();}
+    public ButtonsSelectVariete() {
+        chargerButtons();
+    }
 
-    public static ButtonsInfosLegume getInstance() {
+    public static ButtonsSelectVariete getInstance() {
         if (instance == null) {
-            instance = new ButtonsInfosLegume();
+            instance = new ButtonsSelectVariete();
         }
         return instance;
     }
@@ -29,21 +31,22 @@ public class ButtonsInfosLegume implements Singleton, Serializable {
     public JButton[] getButtons() {
         return buttons;
     }
-    private void chargerIcones(){
+
+    private void chargerIcones() {
         //Ordre des varietes dans l'enum Varietes = ordre des icones dans l'enum IconNames
         for (int i = 0; i < Varietes.values().length; i++) {
             icones[i] = IconRepository.getInstance().getIcone(IconNames.values()[i]);
         }
     }
 
-    private void chargerNoms(){
+    private void chargerNoms() {
         //Ordre = ordre des variétés dans l'enum Varietes
         for (int i = 0; i < Varietes.values().length; i++) {
             noms[i] = Varietes.values()[i].toString();
         }
     }
 
-    private void chargerPrix(){
+    private void chargerPrix() {
         //Ordre = ordre des variétés dans l'enum Varietes
         prix[0] = Float.toString(Salade.PRICE);
         prix[1] = Float.toString(Carotte.PRICE);
@@ -55,7 +58,7 @@ public class ButtonsInfosLegume implements Singleton, Serializable {
         chargerPrix();
         // Ordre des boutons dans le tableau = Ordre des variétés dans l'enum Varietes
         for (int i = 0; i < Varietes.values().length; i++) {
-            buttons[i] = new JButton(noms[i]+" : "+prix[i]+" pièces", icones[i]);
+            buttons[i] = new JButton(noms[i] + " : " + prix[i] + " pièces", icones[i]);
             buttons[i].setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         }
     }
