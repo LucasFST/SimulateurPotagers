68bfb2ce9284021580308fce74b933f303f19d0e
Lucas <lucas.faustmann@gmail.com>
2023-04-15 17:58:23 +0200

    Added tests in VarietesTest for the new Varietes

--- src/test/java/a_vegetable_garden/modele/legumes/VarietesTest.java
+++ src/test/java/a_vegetable_garden/modele/legumes/VarietesTest.java
@@ -6,15 +6,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class VarietesTest {
+class VarietesTest {
     Varietes salade;
     Varietes carotte;
+    Varietes ail;
+    Varietes asperge;
+    Varietes mais;
+    Varietes tomate;
 
     @BeforeEach
     void setUp() {
         salade = new Salade().getVariete();
         carotte = new Carotte().getVariete();
+        ail = new Ail().getVariete();
+        asperge = new Asperge().getVariete();
+        mais = new Mais().getVariete();
+        tomate = new Tomate().getVariete();
+
     }
 
     @Test
@@ -27,5 +35,29 @@ public class VarietesTest {
     @DisplayName("Varietes.CAROTTE should be at the same position as IconNames.CAROTTE")
     void testCarotte() {
         assertEquals(IconNames.CAROTTE.ordinal(), carotte.ordinal());
+    }
+
+    @Test
+    @DisplayName("Varietes.AIL should be at the same position as IconNames.AIL")
+    void testAil() {
+        assertEquals(IconNames.AIL.ordinal(), ail.ordinal());
+    }
+
+    @Test
+    @DisplayName("Varietes.ASPERGE should be at the same position as IconNames.ASPERGE")
+    void testAsperge() {
+        assertEquals(IconNames.ASPERGE.ordinal(), asperge.ordinal());
+    }
+
+    @Test
+    @DisplayName("Varietes.MAIS should be at the same position as IconNames.MAIS")
+    void testMais() {
+        assertEquals(IconNames.MAIS.ordinal(), mais.ordinal());
+    }
+
+    @Test
+    @DisplayName("Varietes.TOMATE should be at the same position as IconNames.TOMATE")
+    void testTomate() {
+        assertEquals(IconNames.TOMATE.ordinal(), tomate.ordinal());
     }
 }
